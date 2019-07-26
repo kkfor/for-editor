8ec161f0da369cdddf06f62b366670484c7406dd
wangyaqi <wangyaqi.chn@foxmail.com>
2019-07-26 18:32:39 +0800

    update: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,3 @@
 module.exports = {
-  // extends: ['eslint-config-alloy/react','eslint-config-alloy/typescript'],
-  extends: ['./lint/es.lint.js', './lint/react.lint.js', './lint/ts.lint.js'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      globalReturn: false,
-      impliedStrict: true,
-      jsx: true
-    }
-  },
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-    commonjs: true,
-    es6: true
-  },
-  rules: {
-  }
+  extends: ['eslint-config-for/react', 'eslint-config-for/typescript']
 }
