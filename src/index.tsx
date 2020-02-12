adce3a00994dee02149bf37a14c04ecf31417a8c
Spoof14 <lars.manden@gmail.com>
2020-02-12 14:17:03 +0100

    Changed IP interface onChange?
    change IP interface to match onChange method implementation

--- src/index.tsx
+++ src/index.tsx
@@ -55,7 +55,7 @@ interface ILeft {
 interface IP {
   value?: string
   lineNum?: number
-  onChange?: (value: string) => void
+  onChange?: (value: string, event: React.ChangeEvent<HTMLTextAreaElement>) => void
   onSave?: (value: string) => void
   placeholder?: string
   fontSize?: string
